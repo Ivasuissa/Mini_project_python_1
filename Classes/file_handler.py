2d4090388deba1e3a3b2357d339fe40bd416066d
Eva SUISSA <evasuissa@MacBook-Pro-de-Eva.local>
2020-04-23 01:15:07 +0300

    q2 and q3 with classes

--- Classes/file_handler.py
+++ Classes/file_handler.py
@@ -1,26 +1,34 @@
 import csv
 
-data_info = []
 
-def load_from_csv(file_name):
+class FileHandler:
 
-    try:
-        with open(file_name, newline='') as csvfile:
-            data =  csv.reader(csvfile, delimiter=',')
-            try:
-                for row in data:
-                    data_info.append(row)
-            except Exception as error:
-                print("There is an error with forloop:")
-                print(error)
-    except Exception as error:
-        print("There is an error with open file:")
-        print(error)
-    return data_info
+    def __init__(self):
+        self.data_info = []
+
+    def load_from_csv(self,file_name):
+
+        try:
+            with open(file_name, newline='') as csvfile:
+                data =  csv.reader(csvfile, delimiter=',')
+                line_count = 0
+                try:
+                    for row in data:
+                        if line_count == 0:
+                            line_count += 1
+                        else:
+                            self.data_info.append(row)
+                except Exception as error:
+                    print("There is an error with forloop:")
+                    print(error)
+        except Exception as error:
+            print("There is an error with open file:")
+            print(error)
+
+        print(self.data_info)
+        return self.data_info
 
 
+file = FileHandler()
+file.load_from_csv("/Users/evasuissa/Desktop/ITC2/Python/day1-alone/Mini_project_python_1/csv_files/user.csv")
 
-
-#load_from_csv("/Users/evasuissa/Desktop/ITC2/Python/day1-alone/csv_files/user.csv")
-
-#print(data_info)
--- Classes/user.py
+++ Classes/user.py
@@ -1,41 +1,45 @@
-from Classes.file_handler import load_from_csv
-
-file_name = "/Users/evasuissa/Desktop/ITC2/Python/day1-alone/Mini_project_python_1/csv_files/user.csv"
-
-def user_auth(name, password):
-
-    name_list = []
-
-    data_info = load_from_csv(file_name)
-
-    try:
-        for item in data_info:
-            name_list.append(item[1])
-
-    except Exception as error:
-        print(error)
-
-    try:
-        if name in name_list:
-            print("yes we found " + name)
-            print(name_list.index(name))
-            name_index = name_list.index(name)
-        else:
-            print("no name in list")
-
-    except Exception as error:
-        print(error)
+from Classes import file_handler
 
 
-    try:
-        if str(password) == data_info[name_index][3]:
-
-            print("password is correct")
-        else:
-            print("password is wrong")
-
-    except Exception as error:
-        print(error)
+FileHandler = file_handler.FileHandler
 
 
-user_auth("amir", "12345678")
+class User:
+    def __init__(self):
+        self.name_list = []
+        self.users = FileHandler()
+
+    def user_auth(self, name, password):
+        self.users.load_from_csv("/Users/evasuissa/Desktop/ITC2/Python/day1-alone/Mini_project_python_1/csv_files/user.csv")
+        try:
+            for item in self.users.data_info:
+                self.name_list.append(item[1])
+
+        except Exception as error:
+            print(error)
+
+        try:
+            if name in self.name_list:
+                print("yes we found " + name)
+
+                name_index = self.name_list.index(name)
+            else:
+                print("no name in list")
+
+        except Exception as error:
+            print(error)
+
+
+        try:
+            if str(password) == self.users.data_info[name_index][3]:
+                print("password is correct")
+            else:
+                print("password is wrong")
+
+        except Exception as error:
+            print(error)
+
+
+user = User()
+role = user.user_auth("amir", "12345678")
+
